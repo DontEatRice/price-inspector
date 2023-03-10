8163c3abb4a733e8239560b791737bb3d4322f62
Jakub Sikorski <jakub.sikorski0211@gmail.com>
2023-03-10 18:53:47 +0100

    styling sidebar ep. 01

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,26 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Sidebar from './sidebar/Sidebar'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Sidebar from './sidebar/Sidebar';
 
 function App() {
-
-  return (
-    <div className="App">
-      Hello World!;
-      <Routes>
-        <Route path='/' element={<Sidebar />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </div>
-  )
+	return (
+		<div className="App">
+			Hello World!
+			<Routes>
+				<Route path="/" element={<Sidebar />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  )
+	return (
+		<div>
+			<h2>Home</h2>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- client/src/sidebar/Sidebar.tsx
+++ client/src/sidebar/Sidebar.tsx
@@ -1,18 +1,36 @@
-import { Link } from "react-router-dom"
+import { useEffect, useRef, useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './Sidebar.css';
 
 function Sidebar() {
-    return (
-        <div className="sidebar-container">
-            <ul>
-                <li>
-                    <Link to="/test">Can your side be crawled?</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
-        </div>
-    )
+	const [visible, setVisible] = useState(false);
+	const hamburger = useRef<HTMLDivElement>(null);
+	// useEffect(() => {
+
+	// }, [visible])
+	console.log(hamburger);
+
+	return (
+		<>
+			<div className="sidebar-container">
+				<div className="sidebar-hamburger" ref={hamburger} onClick={() => setVisible(true)}>
+					<span className="material-symbols-outlined">menu</span>
+				</div>
+				<div className="sidebar-content">
+					<ul>
+						<li>
+							<Link to="/test">Can your side be crawled?</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<Outlet />
+		</>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
